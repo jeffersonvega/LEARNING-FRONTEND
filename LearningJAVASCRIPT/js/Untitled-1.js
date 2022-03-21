4b8f1b4be43c7ab77f674383054e969cb4fc7375
jeffersonvega <jefferson.vega@outlook.es>
2022-03-21 11:39:01 -0500

    1

---
+++ LearningJAVASCRIPT/js/Untitled-1.js
@@ -0,0 +1,5 @@
+var miListaDeCompras=[["cereal",3],["leche",2],["galleta",4],["pan",5],["refresco",7],["pollo",7]];
+
+console.log("Voy a comprar: "+miListaDeCompras[0][1]+" Unidades de: "+miListaDeCompras[0][0]);
+console.log();
+console.log()
--- LearningJAVASCRIPT/js/p1.js
+++
@@ -1,2 +0,0 @@
-alert("HOla a todos");
-
